6236802439f7b43d8f2e2c72698571214dce4abb
FedericaSidoti <federicasidoti21@gmail.com>
2023-11-10 15:38:13 +0100

    ADD- call to server

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <script>
 import PageHeader from './components/PageHeader.vue';
 import PageMain from './components/PageMain.vue';
+import axios from 'axios'
 export default {
   components: {
     PageHeader,
     PageMain,
   },
   data() {
-    return {
-      
+    return {  
+      apikey : '44897644ab70bb16103179e4e3e203dd',
+      queryUser : 'le due torri'
     }
   },
   methods : {
     search(){
-      console.log('ho cercato')
+      axios.get('https://api.themoviedb.org/3/search/movie',{
+        params: {
+          api_key : this.apikey,
+          query : this.queryUser
+        }
+      })
+      .then(res => {
+        console.log(res.data)
+      })
     }
   }  
 }
