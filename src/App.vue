83c36a938311f2ef615324d578da7e82b9275b7a
FedericaSidoti <federicasidoti21@gmail.com>
2023-11-10 14:01:38 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import PageHeader from './components/PageHeader.vue';
+export default {
+  components: {
+    PageHeader,
+  },
+  data() {
+  }
+}
+</script>
+
+<template>
+  <PageHeader/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    components: {
+    },
+    data() {
+    }
+}
+</script>
+
+<template>
+    <header class="page-header">
+        <div class="logo-wrap">
+        <h2>BOOLFLIX</h2>
+    </div>
+    <div class="input-wrap">
+        <input type="text" placeholder="Cerca...">
+        <button>CERCA</button>
+    </div>
+    </header>
+
+</template>
+
+<style>
+
+.page-header{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background-color: black;
+}
+
+.logo-wrap {
+    color: #E50914;
+    font-weight: bold;
+}
+
+</style>
